b705c5a932576e4978aafb8268d9d590491f0545
PrzZiomek2 <pzz64056@gmail.com>
2024-01-25 00:15:49 +0100

    test register and login api route

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -10,6 +10,13 @@ export async function POST(req: Request) {
       const body = await req.json();
       const { name, password, email } = body as RegisterFormValues;
 
+      if(!name || !password || !email) {
+         return NextResponse.json({
+            message: "Uzupełnij wszystkie pola",   
+            status: 500,
+         })
+      };
+
       const headersList = headers();
       const hashedPassword = await bcrypt.hash(password, 10);
       const params: PutCommandInput = {
@@ -22,6 +29,7 @@ export async function POST(req: Request) {
             id: crypto.randomUUID(),
             tokens: 5000,
          },
+         ConditionExpression: "attribute_not_exists(email)",
       };
 
       await ddbDocClient.send(new PutCommand(params));
@@ -30,9 +38,10 @@ export async function POST(req: Request) {
          message: "Rejestracja się udała",
          status: 201,
       });
-   } catch (error) {
+   } 
+   catch (error) {
       return NextResponse.json({
-         message: "Bład podczas rejestracji",
+         message: `Bład podczas rejestracji: ${error}`,
          status: 500,
       });
    }
