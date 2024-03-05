644accc7785862e2de64cb7ac5eeb89e03e0e0e5
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 <pzz64056@gmail.com>
2024-03-05 22:43:19 +0100

    add missing vars

--- sst.config.ts
+++ sst.config.ts
@@ -10,7 +10,19 @@ export default {
    },
    stacks(app) {
       app.stack(function Site({ stack }) {
-         const site = new NextjsSite(stack, "site");
+         const site = new NextjsSite(stack, "Site", {
+            timeout: "30 seconds",
+            memorySize: "2024 MB",
+            environment: {
+               NEXT_PUBLIC_NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL!,
+               NEXT_PUBLIC_GOOGLE_MAPS_KEY:
+                  process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY!,
+               NEXT_PUBLIC_GOOGLE_PLACES_KEY:
+                  process.env.NEXT_PUBLIC_GOOGLE_PLACES_KEY!,
+               NEXT_PUBLIC_GOOGLE_MAP_ID:
+                  process.env.NEXT_PUBLIC_GOOGLE_MAP_ID!,
+            },
+         });
 
          stack.addOutputs({
             SiteUrl: site.url,
