3c0ae707c47ce0fafe03da3250d29a07d1ca6f0a
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 <pzz64056@gmail.com>
2024-04-22 01:44:32 +0200

    change db envs names

--- config/ddbDocClient.ts
+++ config/ddbDocClient.ts
@@ -5,8 +5,8 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 const ddbClient = new DynamoDBClient({
    region: "eu-central-1",
    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+      accessKeyId: process.env.DB_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.DB_SECRET_ACCESS_KEY!,
    },
 });
 
