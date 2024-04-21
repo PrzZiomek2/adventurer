030598597721553f549df7d1ef5fec94c3f2d799
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 <pzz64056@gmail.com>
2024-04-22 01:19:34 +0200

    unify route urls

--- app/_utils/handlersApi.ts
+++ app/_utils/handlersApi.ts
@@ -4,7 +4,7 @@ import { urls } from "./urls";
 const { rootPath } = urls();
 
 export const getServerData = async <T>(url: string): Promise<T> => {
-   const res = await fetch(`${rootPath}/api/${url}`);
+   const res = await fetch(`/api/${url}`);
    const resJson = await res?.json();
    return resJson;
 };
@@ -13,7 +13,7 @@ export const postServerData = async <T extends NextResponseBasic>(
    url: string,
    data: unknown,
 ): Promise<T> => {
-   const res = await fetch(`${rootPath}/api/${url}`, {
+   const res = await fetch(`/api/${url}`, {
       headers: { "Content-Type": "application/json" },
       method: "POST",
       body: serialize(data, { isJSON: true }),
